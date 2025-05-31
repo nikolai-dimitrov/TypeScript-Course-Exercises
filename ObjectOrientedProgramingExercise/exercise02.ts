5bab667422051bc78d6e75083544b6f4285ed600
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-05-31 17:56:19 +0300

    add exercise 2

---
+++ ObjectOrientedProgramingExercise/exercise02.ts
@@ -0,0 +1,33 @@
+class BankAccount {
+	private balance: number;
+	constructor(balance: number) {
+		this.balance = balance;
+	}
+
+	deposit = (amount: number): void => {
+		this.balance += amount;
+	};
+
+	withdraw = (amount: number): void => {
+		this.balance -= amount;
+	};
+
+	getBalance = (): number => {
+		return this.balance;
+	};
+}
+
+const account = new BankAccount(100);
+
+account.deposit(50);
+
+account.withdraw(30);
+
+console.log(account.getBalance());
+
+
+// const account = new BankAccount(20);
+
+// account.withdraw(30);
+
+// console.log(account.getBalance());
