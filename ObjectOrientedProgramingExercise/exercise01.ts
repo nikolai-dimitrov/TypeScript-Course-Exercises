c5930a56365be0aa0a249718dd1a73c3b4ccf32a
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-05-31 17:56:15 +0300

    add exercise 1

---
+++ ObjectOrientedProgramingExercise/exercise01.ts
@@ -0,0 +1,14 @@
+class Person {
+	firstName: string;
+	lastName: string;
+	age: number;
+	constructor(firstName:string, lastName:string, age:number) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+	introduce = (): string => {
+		return `My name is ${this.firstName} ${this.lastName} and I am ${this.age} years old.`;
+	};
+}
