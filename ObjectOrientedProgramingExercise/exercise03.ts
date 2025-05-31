d981ff205758d41d95fc232873e6318dceccb673
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-05-31 18:01:01 +0300

    add exercise 3

---
+++ ObjectOrientedProgramingExercise/exercise03.ts
@@ -0,0 +1,27 @@
+class Vehicle {
+	brand: string;
+
+	constructor(brand: string) {
+		this.brand = brand;
+	}
+
+	drive = (): string => {
+		return `Driving a ${this.brand}`;
+	};
+}
+
+class Car extends Vehicle {
+	model: string;
+	constructor(brand: string, model: string) {
+		super(brand);
+		this.model = model;
+	}
+
+	drive = (): string => {
+		return `Driving a ${this.brand} ${this.model}`;
+	};
+}
+
+const car = new Car("Toyota", "Corolla");
+
+console.log(car.drive());
