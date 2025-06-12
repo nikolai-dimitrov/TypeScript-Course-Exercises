0d110e59eff0dd4ca96bdfa4a05b405c6ecb58cd
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-06-12 17:54:38 +0300

    implement LanguageMessageEncoder methods base functionallity

--- ExamPreparation/src/languageMessageEncoder.ts
+++ ExamPreparation/src/languageMessageEncoder.ts
@@ -1,1 +1,55 @@
-export class LanguageMessageEncoder {}
+import { PartialMessageEncoder } from "./contracts/implemented/partialMessageEncoder";
+
+export class LanguageMessageEncoder<T, V> extends PartialMessageEncoder {
+	static encodedCharsSum: number = 0;
+	static decodedCharsSum: number = 0;
+	static bothEncodedDecodedCharsSum: number = 0;
+	constructor(language: T, cipher: V) {
+		super(language, cipher);
+	}
+	encodeMessage(secretMessage: unknown): string {
+		if (typeof secretMessage !== "string" || secretMessage.length === 0) {
+			return "No message";
+		}
+
+		let strippedMessage = super.stripForbiddenSymbols(secretMessage);
+		LanguageMessageEncoder.encodedCharsSum += strippedMessage.length;
+
+		// TODO: check this.cipher.language.isCompatible or this.language.isCompatible !
+		if (!this.cipher.language.isCompatibleToCharset(strippedMessage)) {
+			return "Message not compatible";
+		}
+
+		return this.cipher.encipher(strippedMessage);
+	}
+
+	decodeMessage(secretMessage: unknown): string {
+		if (typeof secretMessage !== "string" || secretMessage.length === 0) {
+			return "No message";
+		}
+
+		let strippedMessage = super.stripForbiddenSymbols(secretMessage);
+		LanguageMessageEncoder.decodedCharsSum += strippedMessage.length;
+
+		if (!this.cipher.language.isCompatibleToCharset(strippedMessage)) {
+			return "Message not compatible";
+		}
+
+		return this.cipher.decipher(strippedMessage);
+	}
+
+	totalProcessedCharacters(type: "Encoded" | "Decoded" | "Both"): string {
+		let result = 0;
+		if (type === "Encoded") {
+			result = LanguageMessageEncoder.encodedCharsSum;
+		} else if (type === "Decoded") {
+			result = LanguageMessageEncoder.decodedCharsSum;
+		} else if (type === "Both") {
+			result =
+				LanguageMessageEncoder.encodedCharsSum +
+				LanguageMessageEncoder.decodedCharsSum;
+		}
+
+		return `Total processed characters count: ${result}`;
+	}
+}
